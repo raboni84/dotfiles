181dcdcebd5a5ef403184c30fccfbdc1b497a289
dotfiles <dotfiles>
2021-02-28 10:46:40 +0100

    Added more infos to weather plugin

--- .config/awesome/weather.lua
+++ .config/awesome/weather.lua
@@ -15,25 +15,15 @@ module.weatherwidget = wibox.widget {
 }
 
 function module.weather_status()
-    awful.spawn.easy_async_with_shell("curl --compressed http://ip-api.com/line/?fields=192",
+    awful.spawn.easy_async_with_shell("curl -H \"Accept-Language: ${LANG%_*}\" \"wttr.in/?M&format=%f %w %p\"",
         function(stdout, stderr, reason, exit_code)
             if not stdout then
                 module.icon:set_markup("")
                 module.widget:set_markup("")
                 goto continue
             end
-            local region = stdout:gsub("[\r\n]", ","):sub(1, -2)
-            local url = "curl --compressed wttr.in/" .. region .. "?format=\"%f\""
-            awful.spawn.easy_async_with_shell(url, function(stdout, stderr, reason, exit_code)
-                if not stdout then
-                    module.icon:set_markup("")
-                    module.widget:set_markup("")
-                    goto continue2
-                end
-                module.icon:set_markup("  ")
-                module.widget:set_markup(stdout:gsub("+", ""))
-                ::continue2::
-            end)
+            module.icon:set_markup("  ") -- umbrella
+            module.widget:set_markup(stdout:gsub("+", ""):gsub("°C","℃"):gsub("m/s","㎧"):gsub("mm","㎜"))
             ::continue::
         end)
 end
@@ -47,22 +37,18 @@ function module.updateWidget()
 end
 
 module.weatherwidget:buttons(gears.table.join(awful.button({}, 1, function(t)
-    awful.spawn.easy_async_with_shell("curl --compressed http://ip-api.com/line/?fields=192",
+    awful.spawn.easy_async_with_shell("curl -H \"Accept-Language: ${LANG%_*}\" \"http://wttr.in/?2MFT\"",
         function(stdout, stderr, reason, exit_code)
             if not stdout then
                 module.icon:set_markup("")
                 module.widget:set_markup("")
                 goto continue
             end
-            local region = stdout:gsub("[\r\n]", ","):sub(1, -2)
-            local url = "curl -H \"Accept-Language: ${LANG%_*}\" --compressed wttr.in/" .. region .. "?2FT"
-            awful.spawn.easy_async_with_shell(url, function(stdout, stderr, reason, exit_code)
-                    naughty.notify({
-                        text = stdout:sub(1, -2),
-                        font = "DejaVu Sans Mono 9",
-                        timeout = 0
-                    })
-                end)
+            naughty.notify({
+                text = stdout:sub(1, -2),
+                font = "DejaVu Sans Mono 9",
+                timeout = 0
+            })
             ::continue::
         end)
 end)))
