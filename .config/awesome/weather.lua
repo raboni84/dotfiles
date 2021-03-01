94027a795cacf19080d2f302fab09399835eda60
dotfiles <dotfiles>
2021-03-01 03:14:46 +0100

    Added config file for weather location

--- .config/awesome/weather.lua
+++ .config/awesome/weather.lua
@@ -8,14 +8,21 @@ local module = {}
 
 module.icon = wibox.widget.textbox()
 module.widget = wibox.widget.textbox()
+module.location = ""
 module.weatherwidget = wibox.widget {
     module.icon,
     module.widget,
     layout = wibox.layout.align.horizontal
 }
 
+local fd = io.open(gears.filesystem.get_xdg_config_home() .. "weather.loc", "r")
+if fd ~= nil then
+    module.location = fd:read():gsub("[\":/.]","")
+    fd:close()
+end
+
 function module.weather_status()
-    awful.spawn.easy_async_with_shell("curl -H \"Accept-Language: ${LANG%_*}\" \"wttr.in/?M&format=%f %w %p\"",
+    awful.spawn.easy_async_with_shell("curl -H \"Accept-Language: ${LANG%_*}\" \"wttr.in/" .. module.location .. "?M&format=%f %w %p\"",
         function(stdout, stderr, reason, exit_code)
             if not stdout then
                 module.icon:set_markup("")
@@ -37,7 +44,7 @@ function module.updateWidget()
 end
 
 module.weatherwidget:buttons(gears.table.join(awful.button({}, 1, function(t)
-    awful.spawn.easy_async_with_shell("curl -H \"Accept-Language: ${LANG%_*}\" \"http://wttr.in/?2MFT\"",
+    awful.spawn.easy_async_with_shell("curl -H \"Accept-Language: ${LANG%_*}\" \"http://wttr.in/" .. module.location .. "?2MFT\"",
         function(stdout, stderr, reason, exit_code)
             if not stdout then
                 module.icon:set_markup("")
